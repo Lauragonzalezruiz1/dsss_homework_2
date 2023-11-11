a61d7f84012727d10aa8ecc689f3003b06e8d24e
Lauragonzalezruiz1 <87322064+Lauragonzalezruiz1@users.noreply.github.com>
2023-11-11 12:49:41 +0100

    modified setup

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(name='math_quiz',
       version='1.0',
       description='Math game and unit tests',
       author='Larua González Ruiz',
-      url='https://github.com/Lauragonzalezruiz1/dsss_homework_2',
+      url='https://github.com/Lauragonzalezruiz1/dsss_homework_2/tree/main/math_quiz',
       scripts=['math_quiz.py','tests_math_quiz.py']
      )
