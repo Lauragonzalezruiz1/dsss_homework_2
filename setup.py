2a8073c885f63810ca64f76c679f316505bc794a
Lauragonzalezruiz1 <87322064+Lauragonzalezruiz1@users.noreply.github.com>
2023-11-12 15:55:37 +0100

    modified setup

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 
-setup(name='math_quiz',
+setup(name='dsss_homework_2',
       version='1.0',
       description='Math game and unit tests',
       author='Larua González Ruiz',
-      url='https://github.com/Lauragonzalezruiz1/dsss_homework_2/tree/main/math_quiz',
-      scripts=['math_quiz.py','tests_math_quiz.py']
+      url='https://github.com/Lauragonzalezruiz1/dsss_homework_2.git',
+      packages=['math_quiz']
      )
